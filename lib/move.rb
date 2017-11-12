ab47cb70a2e22671fe7c4f50f196786c6c318aab
Melissa Majewski <mreece85@gmail.com>
2017-11-12 21:27:38 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,6 +1,3 @@
-puts "Welcome to Tic Tac Toe!"
-puts "Where would you like to go?"
-
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -9,12 +6,13 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index
-  puts "1".to_i, "2".to_i, "3".to_i, "4".to_i, "5".to_i, "6".to_i, "7".to_i, "8".to_i, "9".to_i
+def input_to_index(user_input)
+  user_input = user_input.to_i
+  user_input = user_input - 1
 end
 
-def move
-
+def move(board, position, char='X')
+  board[position] = char
 end
 
 # code your input_to_index and move method here!
